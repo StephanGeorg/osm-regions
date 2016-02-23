9ccd32c3f7d31afa89f1b82107559f2e5350bb8d
Stephan Georg <s.georg@nearest.place>
2016-02-23 14:52:05 +0100

    Fixed bug w/ wrong bind in getRegion, added test

---
+++ test/osmRegions.js
@@ -0,0 +1,50 @@
+var should = require('should'),
+    OSMRegions  = require('../lib/osm-regions'),
+    API_KEY = 'YOUR_API_KEY';
+
+describe('OSMRegions API Wrapper', function(){
+  var osmregions;
+
+  describe('Initializating', function() {
+
+    it('without any arguments', function() {
+      (function() {
+        osmregions = new OSMRegions();
+      }).should.not.throw();
+    });
+
+
+  });
+
+  describe('API responses', function() {
+
+    beforeEach(function(done){
+      osm = new OSMRegions();
+      done();
+    });
+
+    it('should return rpath of region', function(done) {
+      osm.getRegions({
+        lat: 52.554123413243,
+        lng: 13.213412344
+      }).then(function(res) {
+        res.should.be.a.Array;
+        done();
+      });
+    });
+
+
+    it('should return region', function(done) {
+      osm.getId({
+        id: 51477
+      }).then(function(res) {
+        res.should.be.a.Object;
+        done();
+      })
+
+  });
+
+
+
+
+});
